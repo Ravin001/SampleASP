b27a46ab199c64f32d8e43bc4282d310780b3e6e
Ravinderpal Singh <chahalravin001@gmail.com>
2019-02-06 14:55:06 -0500

    register front end ready

---
+++ SampleASP/Scripts/register.js
@@ -0,0 +1,26 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    // Handler when the DOM is fully loaded
+    var currentDate = new Date();
+    var validAge = currentDate;
+    validAge.setFullYear(currentDate.getFullYear() - 18);
+    $('#DateOfBirth').datetimepicker({
+        format: 'L',
+        maxDate: validAge
+    });
+    accountTypeSelectEl = document.getElementById("AccountType");
+    enableOrDisableCompanyFields();
+    accountTypeSelectEl.addEventListener("change", enableOrDisableCompanyFields);
+
+    function enableOrDisableCompanyFields() {
+        if (accountTypeSelectEl.options[accountTypeSelectEl.selectedIndex].value == "Business") {
+            document.getElementById("FirstNameGroup").style.display = 'none';
+            document.getElementById("LastNameGroup").style.display = 'none';
+            document.getElementById("CompanyNameGroup").style.display = '';
+        }
+        else {
+            document.getElementById("FirstNameGroup").style.display = '';
+            document.getElementById("LastNameGroup").style.display = '';
+            document.getElementById("CompanyNameGroup").style.display = 'none';
+        }
+    }
+});
